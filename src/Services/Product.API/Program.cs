2ad87c4869260f15a8e4c4d7cecf46373b03405a
Tuong Huynh <41580247+huynhdieutuong@users.noreply.github.com>
2023-10-04 07:39:27 +0700

    Create Extensions file

--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,35 +1,20 @@
 using Common.Logging;
+using Product.API.Extensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog(Serilogger.Configure);
 
 Log.Information("Starting Product API up");
 
 try
 {
-    // Add services to the container.
+    builder.Host.UseSerilog(Serilogger.Configure);
+    builder.Host.AddAppConfigurations();
 
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddInfrastructure();
 
     var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
+    app.UseInfrastructure();
     app.Run();
 }
 catch (Exception ex)
