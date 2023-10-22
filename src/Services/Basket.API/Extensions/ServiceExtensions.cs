ca65d82e525c3269f9de4636b19839f76dc59ea0
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2023-10-22 18:30:50 +0700

    Create Extensions file & ConfigureRedis for BasketAPI

---
+++ src/Services/Basket.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Basket.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddControllers();
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+
+            service.ConfigureRedis(configuration);
+            service.AddInfrastructureService();
+        }
+
+        private static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var redisConnectionString = configuration.GetSection("CacheSetttings:ConnectionString").Value;
+            if (string.IsNullOrEmpty(redisConnectionString))
+            {
+                throw new ArgumentException("Redis Conenction string is not configured!");
+            }
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = redisConnectionString;
+            });
+        }
+
+        private static void AddInfrastructureService(this IServiceCollection services)
+        {
+
+        }
+    }
+}
--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,39 +1,30 @@
+using Basket.API.Extensions;
 using Common.Logging;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog(Serilogger.Configure);
 
 Log.Information("Starting Basket API up");
 
 try
 {
-    // Add services to the container.
+    builder.Host.UseSerilog(Serilogger.Configure);
 
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddInfrastructure(builder.Configuration);
 
     var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
+    app.UseInfrastructure();
 
     app.Run();
 }
 catch (Exception ex)
 {
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
     Log.Fatal(ex, "Application terminated unexpectedly");
 }
 finally
