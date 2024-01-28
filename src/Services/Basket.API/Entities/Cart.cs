c973cce3a4c471a4c668e629944a4b8edfa1b3af
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2024-01-28 18:36:22 +0700

    Implement Stock gRPC for Inventory & Consume Stock gRPC from Basket

--- src/Services/Basket.API/Entities/Cart.cs
+++ src/Services/Basket.API/Entities/Cart.cs
@@ -10,13 +10,9 @@
         }
 
         public string UserName { get; set; }
+
         public List<CartItem> Items { get; set; } = new();
-        public decimal TotalPrice
-        {
-            get
-            {
-                return Items.Sum(item => item.ItemPrice * item.Quantity);
-            }
-        }
+
+        public decimal TotalPrice => Items.Sum(item => item.ItemPrice * item.Quantity);
     }
 }
