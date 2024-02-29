329cccfaec7d2c005a3098d7110e461c9b80a663
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2024-02-29 23:45:51 +0700

    Implement SendReminderEmail & DeleteJob

--- src/BuildingBlocks/Contracts/Services/IEmailService.cs
+++ src/BuildingBlocks/Contracts/Services/IEmailService.cs
@@ -3,4 +3,5 @@
 public interface IEmailService<in T> where T : class
 {
     Task SendEmailAsync(T request, CancellationToken cancellationToken = new CancellationToken());
+    void SendEmail(T request);
 }
